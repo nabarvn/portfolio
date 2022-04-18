6d85df782a9a6dbfb64aeed4e3cf2631f048afaa
Nabarun <nicknabarun@gmail.com>
2022-04-18 23:31:00 +0530

    Add CTA and Socials

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import Nabarun from '../../assets/nabarun.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className='container header__container'>
+        <h5>Hello I'm</h5>
+        <h1>Nabarun</h1>
+        <h5 className='text-light'>Frontend Engineer</h5>
+        <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src={Nabarun}/>
+        </div>
+        <a className='scroll__down' href='#contact'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
